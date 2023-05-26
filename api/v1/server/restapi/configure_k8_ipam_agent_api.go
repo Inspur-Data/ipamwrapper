e09524ab6f9bb155478c261d846ebe9157a4da3a
pengbinbin1 <pengbiny@163.com>
2023-05-26 19:58:10 +0800

    add files about server

---
+++ api/v1/server/restapi/configure_k8_ipam_agent_api.go
@@ -0,0 +1,87 @@
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package restapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/Inspur-Data/k8-ipam/api/v1/server/restapi/operations"
+	"github.com/Inspur-Data/k8-ipam/api/v1/server/restapi/operations/health_check"
+	"github.com/Inspur-Data/k8-ipam/api/v1/server/restapi/operations/k8_ipam_agent"
+)
+
+//go:generate swagger generate server --target ../../server --name K8IpamAgentAPI --spec ../../../../home/swagger/openapi.yaml --principal interface{}
+
+func configureFlags(api *operations.K8IpamAgentAPIAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *operations.K8IpamAgentAPIAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	// api.Logger = log.Printf
+
+	api.UseSwaggerUI()
+	// To continue using redoc as your UI, uncomment the following line
+	// api.UseRedoc()
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	if api.K8IpamAgentDeleteIpamHandler == nil {
+		api.K8IpamAgentDeleteIpamHandler = k8_ipam_agent.DeleteIpamHandlerFunc(func(params k8_ipam_agent.DeleteIpamParams) middleware.Responder {
+			return middleware.NotImplemented("operation k8_ipam_agent.DeleteIpam has not yet been implemented")
+		})
+	}
+	if api.HealthCheckGetHealthyHandler == nil {
+		api.HealthCheckGetHealthyHandler = health_check.GetHealthyHandlerFunc(func(params health_check.GetHealthyParams) middleware.Responder {
+			return middleware.NotImplemented("operation health_check.GetHealthy has not yet been implemented")
+		})
+	}
+	if api.K8IpamAgentPostIpamHandler == nil {
+		api.K8IpamAgentPostIpamHandler = k8_ipam_agent.PostIpamHandlerFunc(func(params k8_ipam_agent.PostIpamParams) middleware.Responder {
+			return middleware.NotImplemented("operation k8_ipam_agent.PostIpam has not yet been implemented")
+		})
+	}
+
+	api.PreServerShutdown = func() {}
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix".
+func configureServer(s *http.Server, scheme, addr string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation.
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics.
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
--- go.mod
+++ go.mod
@@ -6,9 +6,13 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/containernetworking/cni v1.1.2
 	github.com/go-openapi/errors v0.20.3
+	github.com/go-openapi/loads v0.21.2
 	github.com/go-openapi/runtime v0.26.0
+	github.com/go-openapi/spec v0.20.8
 	github.com/go-openapi/strfmt v0.21.7
 	github.com/go-openapi/swag v0.22.3
 	github.com/go-openapi/validate v0.22.1
+	github.com/jessevdk/go-flags v1.5.0
 	github.com/pkg/errors v0.9.1
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2
 )
